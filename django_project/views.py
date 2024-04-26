1a7e50fc5c37cc402c43da65b344c2c532efac7f
James Kimata <125971748+Matah002@users.noreply.github.com>
2024-04-26 08:09:43 +0000

    Created a web using apis

--- django_project/views.py
+++ django_project/views.py
@@ -1,6 +1,20 @@
-# iuh3cnkxdebhd3eokbdxkbhnds
-#hjbcemqdunjneqdgvj1kg v1xqiobhwd
-print "hello world"
-#ufjkeioh  endkwjtuf2gjwejlbgita
+from django.shortcuts import render
+import requests
+
+def home(request):
+  response = requests.get('https://api.github.com/events')
+  data = response.json()
+  result = data[0]["repo"]
+
+  response2 = requests.get('https://dog.ceo/api/breeds/image/random')
+  data2 = response2.json()
+  result2 = data2["message"]
+
+  response3 = requests.get('https://api.thecatapi.com/v1/images/search')
+  data3 = response3.json()
+  result3 = data3[0]["url"]
+
+  
+  return render(request, 'templates/index.html', {'result': result, 'result2':result2, 'result3':result3})
 
 
